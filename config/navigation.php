842e653dce693bebe409837dd6a9285e3d585b1f
alex <karpen_ko@hotmail.com>
2019-05-23 16:54:49 +0200

    - navigation: replaced Dashboard menu link with Back link

--- config/navigation.php
+++ config/navigation.php
@@ -4,14 +4,14 @@ $config['navigation_header']['*']['Administration']['children']['Nextcloud'] = a
 		'link' => base_url('index.ci.php/extensions/FHC-Core-Nextcloud/Nextcloud'),
 		'sort' => 20,
 		'description' => 'Nextcloud',
-		'expand' => true
+		'expand' => false
 );
 
 // Add Menu-Entry to Extension Page
 $config['navigation_menu']['extensions/FHC-Core-Nextcloud/*'] = array(
-	'Dashboard' => array(
-		'link' => '#',
-		'description' => 'Dashboard',
-		'icon' => 'dashboard'
+	'Back' => array(
+		'link' => site_url(),
+		'description' => 'Zurück',
+		'icon' => 'angle-left'
 	)
 );
